0aff6b6e8ca0f96ca6e2585b9b70536358c19f95
SurajBKumar93 <surajkumar00244vk@gmail.com>
2026-02-19 00:38:36 +0500

    Add files via upload

--- src/app/page.js
+++ src/app/page.js
@@ -15,9 +15,7 @@ const AmazonSuccessMarquee = dynamic(() => import('@/components/AmazonSuccessMar
 const PortfolioSlider = dynamic(() => import('@/components/PortfolioSlider'), { ssr: true });
 const Testimonials = dynamic(() => import('@/components/Testimonials'), { ssr: true });
 const TrustedByMarquee = dynamic(() => import('@/components/TrustedByMarquee'), { ssr: true });
-const WhyChooseUs = dynamic(() => import('@/components/WhyChooseUs'), { ssr: true });
 const FAQ = dynamic(() => import('@/components/FAQ'), { ssr: true });
-// const CalendlySection = dynamic(() => import('@/components/CalendlySection'), { ssr: true });
 import CalendlySection from '@/components/CalendlySection';
 const BlogPosts = dynamic(() => import('@/components/BlogPosts'), { ssr: true });
 
@@ -31,12 +29,12 @@ export default async function Home() {
       <HeroSlider slides={pageData.hero.slides} />
       <FeaturesGrid features={pageData.features.features} />
       <SuccessStories stories={pageData.successStories.stories} />
-      <ProvenResults />
-      <ServicesAndHowItWorks />
+      <ProvenResults results={pageData.provenResults.results} />
+      <ServicesAndHowItWorks servicesSection={pageData.servicesSection} />
       <AmazonSuccessMarquee />
       <PortfolioSlider items={pageData.portfolio.items} />
       <Testimonials testimonials={pageData.testimonials.testimonials} />
-      <TrustedByMarquee />
+      <TrustedByMarquee brands={pageData.trustedBrands.brands} />
       <FAQ />
       <CalendlySection />
       <BlogPosts posts={pageData.blog.posts} />
