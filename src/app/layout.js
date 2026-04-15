8685812fbde6a586eb1c606534aafcf33bc27963
syed-huzaifa-ali <syedhuzaifaali366@gmail.com>
2026-04-15 16:41:38 +0500

    changes in layout add google scripts

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,6 +22,13 @@ export default function RootLayout({ children }) {
         {/* Google Analytics */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z70N9BEFTX"></script>
         <script>
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-XXXX');`}
+        </script>
+        <script>
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
@@ -70,6 +77,7 @@ export default function RootLayout({ children }) {
         />
       </head>
       <body className={`${hankenGrotesk.variable} antialiased`}>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KGRF2HXN" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <Providers>{children}</Providers>
       </body>
     </html>
